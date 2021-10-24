4d4d63dbb127cbe9db7d791a2a156d0889c77852
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-10-25 08:26:48 +0900

    make the code faster

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -1,29 +1,31 @@
 const http = require('http');
 const url = require('url');
-const querystring = require('querystring')
+const querystring = require('querystring');
 
 const server = http.createServer((req, res)=>{
    const queryObject = url.parse(req.url, true).query;
    let postData = '';
    let ans = [];
+   for (let i = 0; i < 30; i++){
+      ans.push(i+1)
+   }
    req.on('data', (chunk) => {
-      console.log(chunk)
-      postData += chunk
+      postData += chunk;
    }).on('end', () => {
       if (postData){
          const q = url.parse('http://localhost:8080'+req.url+'?'+postData, true)
-         const objData = q.query.pattern
+         const objData = q.query.pattern;
          const queryArray = JSON.parse(objData).obj;
-         for(let i = 1; i < 31; i++){
-            let result = i;
-            for(let j = 0; j < queryArray.length; j++){
-               if (i % queryArray[j].num === 0){
-                  result = queryArray[j].text;
-               }
+         for (let i = 0; i < queryArray.length; i++){
+            let num = queryArray[i].num;
+            let text = queryArray[i].text;
+            let cnt = 1;
+            while (cnt*num < 31){
+               ans[cnt*num-1] = text;
+               cnt++;
             }
-            ans.push(result);
          }
-         const ansString = ans.toString()
+         const ansString = ans.toString();
          res.setHeader('Access-Control-Allow-Origin', '*');
          res.setHeader('Access-Control-Request-Method', '*');
          res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
